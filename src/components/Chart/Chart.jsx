e5a43bcf69fb8846791e0f906725351a9117f3ac
Vishv <krishnakakadiya9@gmai.com>
2020-05-11 20:43:45 +0530

    Second Commit

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -66,26 +66,7 @@ const barChart =(
 
 
 );
-// const barChart = (
-//   confirmed ? (
-//     <Bar
-//       data={{
-//         labels: ['Infected', 'Recovered', 'Deaths'],
-//         datasets: [
-//           {
-//             label: 'People',
-//             backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
-//             data: [confirmed.value, recovered.value, deaths.value],
-//           },
-//         ],
-//       }}
-//       options={{
-//         legend: { display: false },
-//         title: { display: true, text: `Current state in ${country}` },
-//       }}
-//     />
-//   ) : null
-// );
+
 return(
 
  <div className={styles.container}>
